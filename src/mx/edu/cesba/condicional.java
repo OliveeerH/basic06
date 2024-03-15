52fa96704c9036b80ca9e56c39398839ef995d80
OliveeerH <2410177@cesba-queretaro.edu.mx>
2024-03-15 09:58:25 -0600

    -1 commit

---
+++ src/mx/edu/cesba/condicional.java
@@ -0,0 +1,19 @@
+package mx.edu.cesba;
+
+public class condicional {
+    
+    public static void main(String[] args){
+    
+        int mayorEdad = 18;
+        
+        if(mayorEdad >= 18){
+            
+            System.out.println("Si es mayor de edad");
+            
+        }else{
+            System.out.println("Es menor de edad");
+        }
+        
+    }
+    
+}
